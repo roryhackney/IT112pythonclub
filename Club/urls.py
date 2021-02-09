a1b0d15b952014eaa36ddb9d99d36cf9b3492ab7
roryhackney <roryhackney@gmail.com>
2021-02-09 13:53:50 -0800

    resource view commit

--- Club/urls.py
+++ Club/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('resources/', views.getresources, name='resources'),
 ]
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, Minutes, Resource, Event
 
 # Create your views here.
 def index(request):
     return render(request, 'Club/index.html')
+
+def getresources(request):
+    resource_list = Resource.objects.all()
+    return render(request, 'Club/resources.html', {'resource_list': resource_list})

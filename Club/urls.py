8443f22198da20d3f3a2a282abe301faa1a223ee
roryhackney <roryhackney@gmail.com>
2021-02-09 14:49:10 -0800

    Resource Detail commit

--- Club/urls.py
+++ Club/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('resources/', views.getresources, name='resources'),
+    path('resourcedetail/<int:id>', views.resourcedetail, name='detail'),
 ]
--- Club/views.py
+++ Club/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Meeting, Minutes, Resource, Event
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
 def getresources(request):
     resource_list = Resource.objects.all()
     return render(request, 'Club/resources.html', {'resource_list': resource_list})
+
+def resourcedetail(request, id):
+    resource=get_object_or_404(Resource, pk=id)
+    return render(request, 'Club/resourcedetail.html', {'resource': resource})
